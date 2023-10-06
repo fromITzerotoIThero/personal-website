6713253fa4b65905b8c9b20877b35461317c218a
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-06 15:30:21 +0200

    Created basic styleless StackPillsCard component

--- src/components/StackPillsCard/StackPillsCard.jsx
+++ src/components/StackPillsCard/StackPillsCard.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import StackPill from '../StackPill/StackPill';
+import * as logos from '../../assets/logos/logos';
 
 function StackPillsCard() {
     return (
-        <div>
-            <StackPill />
-            <StackPill />
-            <StackPill />
+        <div>           
+            <StackPill 
+                logoComponent={<logos.ReactLogo />}
+                logoLabel="React.js"
+            />
+            <StackPill 
+                logoComponent={<logos.JavaScriptLogo />}
+                logoLabel="JavaScript ES6+"
+            /> 
+            <StackPill 
+                logoComponent={<logos.CSS3Logo />}
+                logoLabel="CSS3"
+            /> 
+            <StackPill 
+                logoComponent={<logos.HTML5Logo />}
+                logoLabel="HTML5"
+            /> 
+            <StackPill 
+                logoComponent={<logos.GitLogo />}
+                logoLabel="Git"
+            /> 
+            <StackPill 
+                logoComponent={<logos.GitHubLogo />}
+                logoLabel="GitHub"
+            /> 
+            <StackPill 
+                logoComponent={<logos.FigmaLogo />}
+                logoLabel="Figma"
+            />  
+            <StackPill 
+                logoComponent={<logos.GIMPLogo />}
+                logoLabel="GIMP"
+            />  
+            <StackPill 
+                logoComponent={<logos.VSCodeLogo />}
+                logoLabel="VS Code"
+            />  
         </div>
     );
 }
