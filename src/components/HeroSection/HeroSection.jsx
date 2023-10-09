43fa19ebcb0a1d87118f3bb1902547141734141f
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-09 22:30:11 +0200

    Experiments with neon-like HeroSection

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './HeroSection.module.css';
 import Menu from '../Menu/Menu';
 
-function HeroSection() {
+function HeroSection({}) {
     return (
         <header className={styles.container}>
             <Menu />
@@ -12,3 +12,20 @@ function HeroSection() {
 }
 
 export default HeroSection;
+
+
+
+// import React from 'react';
+// import styles from './HeroSection.module.css';
+// import Menu from '../Menu/Menu';
+
+// function HeroSection() {
+//     return (
+//         <header className={styles.container}>
+//             <Menu />
+//             <h1 className={styles.heading}>Gosia Panek - Junior Front-End Dev</h1>
+//         </header>
+//     );
+// }
+
+// export default HeroSection;
---
+++ src/components/HeroSectionAlternative/HeroSectionAlternative.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './HeroSectionAlternative.module.css';
+import Menu from '../Menu/Menu';
+
+function HeroSectionAlternative() {
+    return (
+        <header className={styles.container}>
+            <Menu />
+            <h1 className={styles.heading}>Gosia Panek - Junior Front-End Dev</h1>           
+        </header>
+    );
+}
+
+export default HeroSectionAlternative;
+
