542463ea675e2fe6bcea89b369c9f1f3ab1adb2d
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-09 22:32:43 +0200

    Added extra content to ProfileCards

--- src/components/ProfileCardsSection/ProfileCardsSection.jsx
+++ src/components/ProfileCardsSection/ProfileCardsSection.jsx
@@ -9,7 +9,9 @@ function ProfileCardsSection() {
         <section className={styles.container}>
             <ProfileCard
                 heading="About"
-                content="I'm a former medical doctor. It took me quite a lot of time to figure out what my dream profession is, but I'm finally there XD"
+                content="I'm a former medical doctor. It took me quite a lot of time to figure out what my dream profession is,
+                but now that I've found it, it just feels so good! 
+                Front-end development - <3UTTMAB!!!"
                 imageSrc={LogoBee}
             />
             <ProfileCard
@@ -19,8 +21,15 @@ function ProfileCardsSection() {
                 }
             />
             <ProfileCard
-                heading="About"
-                content="I'm a former medical doctor. It took me quite a lot of time to figure out what my dream profession is, but I'm finally there XD"
+                heading="What's unique about me?"
+                content={
+                    <ul>
+                        <li>I love working with people and I'm good at it!</li>
+                        <li>It's not just that I speak English well, I really like communicating in English :{")"}</li>
+                        <li>"DIG DEEPER!" is my motto when it comes down to understanding things - 
+                            I strive for a profound understanding of concepts and languages - that why I love MDN and all sorts of docs</li>
+                    </ul>
+                }
             />
         </section>
     );
