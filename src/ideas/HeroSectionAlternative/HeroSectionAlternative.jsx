9fd2d06d5ab83629e2a3b768df63c46c921e123d
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-10 13:52:23 +0200

    Remove neon-like HeroSection & move to ideas

--- src/ideas/HeroSectionAlternative/HeroSectionAlternative.jsx
+++ src/ideas/HeroSectionAlternative/HeroSectionAlternative.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './HeroSectionAlternative.module.css';
-import Menu from '../Menu/Menu';
+import Menu from '../../components/Menu/Menu';
 
 function HeroSectionAlternative() {
     return (
