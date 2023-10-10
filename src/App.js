912ad0c2ac7f3551cc6729320ce6159dfa687d2b
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-10 13:58:06 +0200

    Remove commented-out code from App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { useState } from 'react';
 import styles from './App.css';
 import HeroSection from './components/HeroSection/HeroSection';
-import HeroSectionAlternative from './components/HeroSectionAlternative/HeroSectionAlternative';
 import ProfileCardsSection from './components/ProfileCardsSection/ProfileCardsSection';
 import Footer from './components/Footer/Footer';
 
 function App() {
-    // const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-    // const toggleMobileMenu = () => {
-    //     setIsMobileMenuOpen(!isMobileMenuOpen);
-    //     const contentContainer = document.querySelector("#content_container");
-
-    //     if (contentContainer) {
-    //         contentContainer.style.overflow = isMobileMenuOpen ? "hidden" : "auto";
-    //     }
-    // };
-    
 
     return (
         <div id="content_container" className={styles.content_container}>
             <HeroSection />
-            <HeroSectionAlternative />
             <ProfileCardsSection />
             <Footer />
         </div>
@@ -30,28 +16,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import React from 'react';
-// import { useState } from 'react';
-// import './App.css';
-// import HeroSection from './components/HeroSection/HeroSection';
-// import ProfileCardsSection from './components/ProfileCardsSection/ProfileCardsSection';
-// import Footer from './components/Footer/Footer';
-
-// function App() {
-//     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-    
-
-//     return (
-//         <>
-//             <HeroSection />
-//             <ProfileCardsSection />
-//             <Footer />
-//         </>
-//     );
-// }
-
-// export default App;
