966920f67ca2ebefa4c1328eabcffded672981df
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-12 13:26:46 +0200

    Add & import portfolio websites printscreens

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -2,5 +2,8 @@ import rubberDuckImage from './s-tsuchiya-rubber-duck-unsplash.jpg';
 import rubberDuckInBathImage from './thomas-despeyroux-rubber-duck-unsplash.jpg';
 import codeScreenBrightImage from './shahadat-rahman-code-screen-unsplash.jpg';
 import codeScreenDarkImage from './thomas-tastet-code-screen-unsplash.jpg';
+import gosiaPanekWebsitePrintscreen from './gosia-panek-website.png';
+import anexWebsitePrintscreen from './anex-website.png';
+import promaWebsitePrintscreen from './proma-website.png';
 
-export { rubberDuckImage, rubberDuckInBathImage, codeScreenBrightImage, codeScreenDarkImage };
+export { gosiaPanekWebsitePrintscreen, anexWebsitePrintscreen, promaWebsitePrintscreen, rubberDuckImage, rubberDuckInBathImage, codeScreenBrightImage, codeScreenDarkImage };
