09cd51a9e2bee6df117af2c8d8d92e8be4af94f6
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-12 11:26:54 +0200

    Create images.js for easier import of images

---
+++ src/assets/images/images.js
@@ -0,0 +1,6 @@
+import rubberDuckImage from './s-tsuchiya-rubber-duck-unsplash.jpg';
+import rubberDuckInBathImage from './thomas-despeyroux-rubber-duck-unsplash.jpg';
+import codeScreenBrightImage from './shahadat-rahman-code-screen-unsplash.jpg';
+import codeScreenDarkImage from './thomas-tastet-code-screen-unsplash.jpg';
+
+export { rubberDuckImage, rubberDuckInBathImage, codeScreenBrightImage, codeScreenDarkImage };
