277eafaf603f91c552be15b2a31059a9f034bb91
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-12 13:27:28 +0200

    Change to class component due to malfunction

--- src/components/PortfolioSlider/PortfolioSlider.jsx
+++ src/components/PortfolioSlider/PortfolioSlider.jsx
@@ -1,54 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import styles from './PortfolioSlider.module.css';
-import duck from '../../assets/images/thomas-despeyroux-rubber-duck-unsplash.jpg';
+import { anexWebsitePrintscreen, gosiaPanekWebsitePrintscreen, promaWebsitePrintscreen } from '../../assets/images/images';
 
+class PortfolioSlider extends Component {
 
-function PortfolioSlider() {
-    const settings = {
-        dots: true,
-        speed: 500,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 400,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    centerMode: true,
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                }
-            }
-        ],
-    };
+    render() {
 
-    return (
-        <div className={styles.container}>
-            <Slider {...settings} className={styles.slider}>
-                <div>
-                    <img src={duck} alt="Ducky duck" className={styles.image} />
-                </div>
-                <div>
-                    <img src={duck} alt="Ducky duck" />
-                </div>
-                <div>
-                    <img src={duck} alt="Ducky duck" />
-                </div>
-            </Slider>
-        </div>
-    );
+        const breakpoint = {
+            small: 400,
+            medium: 768, 
+            large: 1024,
+        }
+
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: breakpoint.large,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        centerMode: true,
+                    }
+                },
+                // {
+                //     breakpoint: breakpoint.medium,
+                //     settings: {
+                //         slidesToShow: 1,
+                //     }
+                // },
+            ],
+        };
+
+        return (
+            <div className={styles.container}>
+                <Slider {...settings} className={styles.slider}>
+                    <div className={styles.image_container}>
+                        <img src={anexWebsitePrintscreen} alt="Ducky duck" className={styles.image} />
+                    </div>
+                    <div className={styles.image_container}>
+                        <img src={gosiaPanekWebsitePrintscreen} alt="Ducky duck" className={styles.image} />
+                    </div>
+                    <div className={styles.image_container}>
+                        <img src={promaWebsitePrintscreen} alt="Ducky duck" className={styles.image} />
+                    </div>
+                </Slider>
+            </div >
+        );
+    }
 }
 
 export default PortfolioSlider;
