b1d022c85226abde32417053679661b346fda32b
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-16 15:52:28 +0200

    Create InformationPill (for ProfileCards) - WIP

---
+++ src/components/InformationPill/InformationPill.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './InformationPill.module.css';
+
+function InformationPill({ icon, text }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.icon}>{icon}</div>
+            <p className={styles.text}>{text}</p>
+        </div>
+    );
+}
+
+export default InformationPill;
