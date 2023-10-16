2968c7f049058761d43daddba9e943bcf8a176a6
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-16 15:53:04 +0200

    Test different content for ProfileCardsSection

--- src/components/ProfileCardsSection/ProfileCardsSection.jsx
+++ src/components/ProfileCardsSection/ProfileCardsSection.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import styles from './ProfileCardsSection.module.css';
+// import * as icons from '../../assets/icons/icons';
+// import Stethoscope from '../../assets/icons/stethoscope-icon.svg';
 import ProfileCard from '../ProfileCard/ProfileCard';
+// import InformationPill from '../InformationPill/InformationPill';
 import StackPillsCard from '../StackPillsCard/StackPillsCard';
-import LogoBee from "../../logo-bee-no-background.svg";
 
 function ProfileCardsSection() {
     return (
         <section className={styles.container}>
             <ProfileCard
                 heading="About"
-                content="I'm a former medical doctor. It took me quite a lot of time to figure out what my dream profession is,
-                but now that I've found it, it just feels so good! 
-                Front-end development - <3UTTMAB!!!"
-                imageSrc={LogoBee}
+                content="a"
             />
             <ProfileCard
                 heading="Stack"
@@ -22,14 +21,7 @@ function ProfileCardsSection() {
             />
             <ProfileCard
                 heading="What's unique about me?"
-                content={
-                    <ul>
-                        <li>I love working with people and I'm good at it!</li>
-                        <li>It's not just that I speak English well, I really like communicating in English :{")"}</li>
-                        <li>"DIG DEEPER!" is my motto when it comes down to understanding things - 
-                            I strive for a profound understanding of concepts and languages - that why I love MDN and all sorts of docs</li>
-                    </ul>
-                }
+                content="b"
             />
         </section>
     );
