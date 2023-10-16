ad52708d7e3d4f5a9492c41e644456af232ec9c4
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-16 12:14:49 +0200

    Add "work in progress" stripe to StackPill

--- src/components/StackPill/StackPill.jsx
+++ src/components/StackPill/StackPill.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './StackPill.module.css';
 
-function StackPill({ logoComponent, logoLabel }) {
+function StackPill({ logoComponent, logoLabel, isInProgress }) {
     return (
         <div className={styles.container}>
+            {isInProgress &&
+                <div className={styles.progress_band}>WIP</div>
+            }
+
             <div className={styles.logo}>{logoComponent}</div>
             <p className={styles.label}>{logoLabel}</p>
         </div>
