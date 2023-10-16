a36722b0284351ca1bacd8765712ccda02d6fc8d
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-16 15:51:27 +0200

    Add & export icons from SVGrepo

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,6 @@
+export { ReactComponent as EnglishLanguageIcon } from './english-lang-icon.svg';
+export { ReactComponent as StethoscopeIcon } from './stethoscope-icon.svg';
+export { ReactComponent as LaptopIcon } from './laptop-icon.svg';
+export { ReactComponent as GroupOfPeopleIcon } from './group-of-people-icon.svg';
+export { ReactComponent as MagnifyingGlassIcon } from './magnifying-glass-icon.svg';
+export { ReactComponent as RightArrowIcon } from './right-arrow-icon.svg';
