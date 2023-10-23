2303032fd9519b9841024b02b7008e6c25d86e3f
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-23 13:22:46 +0200

    Create initial ContactSection component

---
+++ src/components/ContactSection/ContactSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ContactSection.module.css';
+
+function ContactSection() {
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.heading}>Contact me</h1>
+        </div>
+    );
+}
+
+export default ContactSection;
