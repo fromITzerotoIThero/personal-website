91d2d715ad79dbe52aaec3850b48ffcda7010314
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-23 14:06:51 +0200

    Add and export contact icons

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -5,3 +5,5 @@ export { ReactComponent as GroupOfPeopleIcon } from './group-of-people-icon.svg'
 export { ReactComponent as MagnifyingGlassIcon } from './magnifying-glass-icon.svg';
 export { ReactComponent as ArrowRightIcon } from './arrow-right-icon.svg';
 export { ReactComponent as ArrowDownIcon } from './arrow-down-icon.svg';
+export { ReactComponent as EmailIcon } from './email-icon.svg';
+export { ReactComponent as TelephoneIcon } from './telephone-icon.svg';
