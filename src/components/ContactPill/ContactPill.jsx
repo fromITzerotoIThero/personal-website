56f7a64bdc418417b2ebf8bb0b4c8fdca35ee6fc
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-24 12:22:42 +0200

    Add props to ContactPill for more functionality

--- src/components/ContactPill/ContactPill.jsx
+++ src/components/ContactPill/ContactPill.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './ContactPill.module.css';
 
-function ContactPill({ href, ariaLabel, iconComponent, contactInformation }) {
+function ContactPill({ href, ariaLabel, iconComponent, contactInformation, download = false, target = "_blank"}) {
     return (
-        <a href={href} aria-label={ariaLabel} className={styles.container}>
+        <a href={href} aria-label={ariaLabel} className={styles.container} download={download} target={target} rel="noopener noreferrer">
             <div className={styles.icon_container}>{iconComponent}</div>
             <div className={styles.text}>{contactInformation}</div>
         </a>
