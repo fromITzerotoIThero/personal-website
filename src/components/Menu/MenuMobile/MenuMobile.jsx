bf10cddc7092121e0d184fe3b03b819a19bc0f15
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-24 15:43:23 +0200

    Use Flyout & Hamburger components in MenuMobile

--- src/components/Menu/MenuMobile/MenuMobile.jsx
+++ src/components/Menu/MenuMobile/MenuMobile.jsx
@@ -1,172 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
-import styles from './MenuMobile.module.css';
-import MenuMobileItem from './MenuMobileItem/MenuMobileItem';
+import MenuMobileFlyout from './MenuMobileFlyout/MenuMobileFlyout';
+import MenuMobileHamburger from './MenuMobileHamburger/MenuMobileHamburger';
 
 function MenuMobile() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuMobileOpen, setIsMenuMobileOpen] = useState(false);
 
     const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+        setIsMenuMobileOpen(!isMenuMobileOpen);
     };
 
     const closeMenu = () => {
-        setIsMenuOpen(false);
+        setIsMenuMobileOpen(false);
     };
 
     return (
         <>
-            {isMenuOpen ?
-                <div className={styles.menu_overlay}>
-                    <nav className={styles.open}>
-                        <button
-                            className={`${styles.button} ${styles.close_menu}`}
-                            onClick={closeMenu}>
-                            X CLOSE
-                        </button>
-                        <ul>
-                            <MenuMobileItem
-                                itemName="About"
-                            />
-                            <MenuMobileItem
-                                itemName="Blabla"
-                            />
-                            <MenuMobileItem
-                                itemName="Portfolio"
-                            />
-                            <MenuMobileItem
-                                itemName="Contact"
-                            />
-                            <MenuMobileItem
-                                itemName="About"
-                            />
-                            <MenuMobileItem
-                                itemName="Blabla"
-                            />
-                            <MenuMobileItem
-                                itemName="Portfolio"
-                            />
-                            <MenuMobileItem
-                                itemName="Contact"
-                            />
-                        </ul>
-                    </nav>
-                </div>
+            {isMenuMobileOpen ?
+                <MenuMobileFlyout
+                    onCloseHandler={closeMenu}
+                />
                 :
-                <nav className={styles.closed}>
-                    <button
-                        className={`${styles.button} ${styles.open_menu}`}
-                        onClick={toggleMenu}>
-                        MENU
-                    </button>
-                    {/* <MenuMobileItem
-                            itemName="About"
-                        />
-                        <MenuMobileItem
-                            itemName="Blabla"
-                        />
-                        <MenuMobileItem
-                            itemName="Portfolio"
-                        />
-                        <MenuMobileItem
-                            itemName="Contact"
-                        /> */}
-                </nav>
+                <MenuMobileHamburger
+                    onClickHandler={toggleMenu}
+                />
             }
         </>
     );
 }
 
 export default MenuMobile;
-
-
-// import React from 'react';
-// import { useState } from 'react';
-// import styles from './MenuMobile.module.css';
-// import MenuMobileItem from '../MenuMobileItem/MenuMobileItem';
-
-// function MenuMobile() {
-//     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-//     const toggleMenu = () => {
-//         setIsMenuOpen(!isMenuOpen);
-//     };
-
-//     const closeMenu = () => {
-//         setIsMenuOpen(false);
-//     };
-
-//     return (
-//         <>
-//             {isMenuOpen ?
-//                 <nav className={styles.open}>
-//                     <button
-//                         className={`${styles.button} ${styles.close_menu}`}
-//                         onClick={closeMenu}>
-//                         X CLOSE
-//                     </button>
-//                     <ul>
-//                         <MenuMobileItem
-//                             itemName="About"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Blabla"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Portfolio"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Contact"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="About"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Blabla"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Portfolio"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Contact"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="About"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Blabla"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Portfolio"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Contact"
-//                         />
-//                     </ul>
-//                 </nav>
-//                 :
-//                 <nav className={styles.closed}>
-//                     <button
-//                         className={`${styles.button} ${styles.open_menu}`}
-//                         onClick={toggleMenu}>
-//                         MENU
-//                     </button>
-//                     {/* <MenuMobileItem
-//                             itemName="About"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Blabla"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Portfolio"
-//                         />
-//                         <MenuMobileItem
-//                             itemName="Contact"
-//                         /> */}
-//                 </nav>
-//             }
-//         </>
-//     );
-// }
-
-// export default MenuMobile;
