11b55ea8f35b3d63c43e56e03e973a08b7223eec
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-25 09:00:13 +0200

    Refactor ProfileCard

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -5,7 +5,7 @@ function ProfileCard({ heading, content }) {
     return (
         <div className={styles.container}>
             <h1 className={styles.heading}>{heading}</h1>
-            <p className={styles.content}>{content}</p>
+            <div className={styles.content}>{content}</div>
         </div>
     );
 }
