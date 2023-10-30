5b7c6cc1111ceb10d1a11f5f5117dc422c39fbc0
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-10-30 14:36:19 +0100

    Add ErrorPage to router config

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
     },
 ]);
 
