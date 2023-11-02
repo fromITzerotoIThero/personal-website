0a8e11d50d93164a977277ffba8c4d7b7c02d3fb
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-11-02 10:01:02 +0100

    Refactor MenuDesktop with useScrollPosition hook

--- src/components/Menu/MenuDesktop/MenuDesktop.jsx
+++ src/components/Menu/MenuDesktop/MenuDesktop.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import styles from './MenuDesktop.module.css';
 import MenuDesktopItem from './MenuDesktopItem/MenuDesktopItem';
 
 function MenuDesktop() {
+    const [isScrolled, setIsScrolled] = useState(false);
+    useScrollPosition(({ currPos }) => {
+        if (currPos.y < - 400) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    },
+        []
+    );
+
+    const menuContainerClass = isScrolled ?
+        `${styles.container_external} ${styles.scrolled}`
+        :
+        styles.container_external;
+
     return (
-        <div className={styles.container_external}>
+        <div className={menuContainerClass}>
             <nav className={styles.container}>
                 <ul className={styles.list}>
                     <MenuDesktopItem
