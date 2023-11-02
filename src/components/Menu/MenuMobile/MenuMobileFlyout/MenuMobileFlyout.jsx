4f324693e61cdb579d13c0eb1f217a39956703ff
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-11-02 09:02:21 +0100

    Add entries to MenuMobileFlyout

--- src/components/Menu/MenuMobile/MenuMobileFlyout/MenuMobileFlyout.jsx
+++ src/components/Menu/MenuMobile/MenuMobileFlyout/MenuMobileFlyout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './MenuMobileFlyout.module.css';
 import MenuMobileItem from '../MenuMobileItem/MenuMobileItem';
 
-function MenuMobileFlyout({onCloseHandler}) {
+function MenuMobileFlyout({ onCloseHandler }) {
     return (
         <div className={styles.overlay} >
             <nav className={styles.navigation}>
@@ -17,11 +17,45 @@ function MenuMobileFlyout({onCloseHandler}) {
                         linkTarget="/#about"
                         onMenuItemClick={onCloseHandler}
                     />
+                    <ul className={styles.submenu}>
+                        <MenuMobileItem
+                            itemName="Intro"
+                            linkTarget="/#intro"
+                            onMenuItemClick={onCloseHandler}
+                        />
+                        <MenuMobileItem
+                            itemName="Stack"
+                            linkTarget="/#stack"
+                            onMenuItemClick={onCloseHandler}
+                        />
+                        <MenuMobileItem
+                            itemName="Extras"
+                            linkTarget="/#extras"
+                            onMenuItemClick={onCloseHandler}
+                        />
+                    </ul>
                     <MenuMobileItem
                         itemName="Portfolio"
                         linkTarget="/#portfolio"
                         onMenuItemClick={onCloseHandler}
                     />
+                    <ul className={styles.submenu}>
+                        <MenuMobileItem
+                            itemName="IBOS"
+                            linkTarget="/#portfolio"
+                            onMenuItemClick={onCloseHandler}
+                        />
+                        <MenuMobileItem
+                            itemName="Anex"
+                            linkTarget="/#portfolio"
+                            onMenuItemClick={onCloseHandler}
+                        />
+                        <MenuMobileItem
+                            itemName="ProMa"
+                            linkTarget="/#portfolio"
+                            onMenuItemClick={onCloseHandler}
+                        />
+                    </ul>
                     <MenuMobileItem
                         itemName="Contact"
                         linkTarget="/#contact"
