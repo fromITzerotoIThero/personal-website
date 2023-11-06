20af65bbef3ff19b3e4bf0f102f0fabd9988922a
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-11-06 16:11:04 +0100

    Add new draft paths to routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ProjectPage from './components/ProjectPage/ProjectPage';
 import ErrorPage from './components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
@@ -10,6 +11,18 @@ const router = createBrowserRouter([
         path: '/',
         element: <App />,
         errorElement: <ErrorPage />,
+    },
+    {
+        path: '/portfolio',
+        element: <ProjectPage />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: '/:project',
+                element: <ProjectPage />,
+                errorElement: <ErrorPage />,
+            }
+        ],
     },
 ]);
 
