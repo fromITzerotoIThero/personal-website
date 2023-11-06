7feacc068fee1b4076172ec2dc7eaac4ab102241
fromITzerotoIThero <mpanek.uni@gmail.com>
2023-11-06 16:10:34 +0100

    Create draft ProjectPage, add mockup to Slider

--- src/components/PortfolioSlider/PortfolioSlider.jsx
+++ src/components/PortfolioSlider/PortfolioSlider.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from './PortfolioSlider.module.css';
-import { personalWebsiteMockup, anexWebsitePrintscreen, gosiaPanekWebsitePrintscreen, promaWebsitePrintscreen } from '../../assets/images/images';
+import { personalWebsiteMockup, anexWebsitePrintscreen, IBOSHomePageMockup, promaWebsitePrintscreen } from '../../assets/images/images';
 
 class PortfolioSlider extends Component {
 
@@ -19,13 +20,15 @@ class PortfolioSlider extends Component {
 
         return (
             <section className={styles.container}>
-                <div  id="portfolio" className={styles.navigation_trick_element}></div>
+                <div id="portfolio" className={styles.navigation_trick_element}></div>
                 <Slider {...settings} className={styles.slider}>
+                    <Link to="/portfolio/ibos">
+                        <div className={styles.slide}>
+                            <img src={IBOSHomePageMockup} alt="IBOS app on mobile screen" className={styles.image} />
+                        </div>
+                    </Link>
                     <div className={styles.slide}>
                         <img src={anexWebsitePrintscreen} alt="Ducky duck" className={styles.image} />
-                    </div>
-                    <div className={styles.slide}>
-                        <img src={gosiaPanekWebsitePrintscreen} alt="Ducky duck" className={styles.image} />
                     </div>
                     <div className={styles.slide}>
                         <img src={promaWebsitePrintscreen} alt="Ducky duck" className={styles.image} />
---
+++ src/components/ProjectPage/ProjectPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProjectPage() {
+    return (
+        <div>Hi there! I'm your ProjectPage!</div>
+    );
+}
+
+export default ProjectPage;
